5c352a4d85a47d2e8509cbaa09d3bdab181ddf15
jasonliu119 <weijiejason119@gmail.com>
2017-11-02 17:35:24 -0700

    add allowed_hosts

--- tongchi/settings.py
+++ tongchi/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'tongchi.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tongchi_db',
-        'USER': 'admin',
-        'PASSWORD': '123',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'tongchi_db',
+        # 'USER': 'admin',
+        # 'PASSWORD': '123',
+        # 'HOST': '',
+        # 'PORT': '',
     }
 }
 
